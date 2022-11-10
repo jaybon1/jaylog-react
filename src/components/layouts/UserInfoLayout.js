8211c0e9cd22b22102e4affc214fa9fc30439941
jaybon1 <ponggas2@gmail.com>
2022-11-11 00:19:03 +0900

    로그인레이아웃 수정

--- src/components/layouts/UserInfoLayout.js
+++ src/components/layouts/UserInfoLayout.js
@@ -7,8 +7,7 @@ const UserInfoLayout = ({ children }) => {
     <section style={{ backgroundColor: "#508bfc", minHeight: "100vh" }}>
       <Container className="py-5 h-100">
         <Row className="d-flex justify-content-center align-items-center h-100">
-          <Col className="col-12 col-md-8 col-lg-6 col-xl-5"></Col>
-          {children}
+          <Col className="col-12 col-md-8 col-lg-6 col-xl-5">{children}</Col>
         </Row>
       </Container>
     </section>
